a9dc4e4c9db5f8db36fd72261897c434b3112fc0
AndySprinkle <Andy@An-Existing-Computer.local>
2017-03-08 16:20:17 -0500

    What we did on the first day of class

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+createCanvas(800,800); 
+}
+function draw() {
+  background( mouseX, 200, 175, 150 , 100, 50, 0);
+	background( mouseY, 200, 175, 150 , 100, 50, 0);
+  rect( mouseX, mouseY, 50, 100, 150, 200);
+ 
+}
+
+
